8211d07011ada16f4c4c71f19175081b61241c28
Antoine GALOPIN <galopinantoine.contact@gmail.com>
2026-05-02 23:14:14 +0200

    Added multication asm example

---
+++ examples/asm/multiply.asm
@@ -0,0 +1,17 @@
+; Multiply 3 by 4 using repeated addition
+; Result will be stored in R3
+
+start:
+LOAD R1 3      ; multiplicand
+LOAD R2 4      ; multiplier
+LOAD R3 0      ; result
+LOAD R4 1      ; constant 1
+
+loop:
+ADD R3 R3 R1   ; result = result + multiplicand
+SUB R2 R2 R4   ; multiplier = multiplier - 1
+JUMPIF end     ; if multiplier == 0, stop
+JUMP loop      ; otherwise continue
+
+end:
+HALT
